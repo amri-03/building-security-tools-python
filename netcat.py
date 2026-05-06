2ebc164dc1585b8769d4fc143d042dc7a013b9df
amri-03 <amanrkumar030@gmail.com>
2026-05-06 11:41:30 -0400

    Improved execute mode connection handling

--- netcat.py
+++ netcat.py
@@ -5,155 +5,184 @@ import argparse
 import subprocess
 
 def parse_args():
-	parser = argparse.ArgumentParser(description="Netcat Tool")
+        parser = argparse.ArgumentParser(description="Netcat Tool")
 
-	parser.add_argument("-c", "--command", action=store_true, help="command shell")
-	parser.add_argument("-t", "--target", default="0.0.0.0", help="specified IP")
-	parser.add_argument("-p", "--port", type=int, default=5555, help="specified Port")
-	parser.add_argument("-l", "--listen", action="store_true", help="listen")
-	parser.add_argument("-e", "--execute", help="execute commands")
-	parser.add_argument("-u", "--upload", help="upload a file")
+        parser.add_argument("-c", "--command", action="store_true", help="command shell")
+        parser.add_argument("-t", "--target", default="0.0.0.0", help="specified IP")
+        parser.add_argument("-p", "--port", type=int, default=5555, help="specified port")
+        parser.add_argument("-l", "--listen", action="store_true", help="listen")
+        parser.add_argument("-e", "--execute", help="execute specified command")
+        parser.add_argument("-u", "--upload", help="upload file")
 
-	return parser.parse_args()
+        return parser.parse_args()
 
 class Netcat:
-	def __init__(self, args):
-		self.args = args
-		self.buffer = b""
+        def __init__(self, args):
+                self.args = args
+                self.buffer = b""
 
-	def send(self):
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.socket.connect((self.args.target, self.args.port))
+        def send(self):
+                self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.socket.connect((self.args.target, self.args.port))
 
-		try:
-			if self.buffer:
-				self.socket.send(self.buffer)
+                print("[*] Connected to server...")
 
-			while True:
-				response = b""
+                try:
+                        if self.buffer:
+                                self.socket.send(self.buffer)
 
-				while b"<END>" not in response:
-					data = self.socket.recv(4096)
-					if not data:
-						print("Connection closed by server.")
-						self.socket.close()
-						return
-					response += data
+                        while True:
+                                response = b""
 
-				print(response.replace(b"<END>", b"").decode(), end="")
+                                while b"<END>" not in response:
+                                        data = self.socket.recv(4096)
 
-				buffer = input("") + "\n"
+                                        if not data:
+                                                print("\nConnection closed by server.")
+                                                self.socket.close()
+                                                return
 
-				if buffer.lower() in ["exit", "quit", "q"]:
-					self.socket.send((buffer + "\n").encode())
-					print("Closing connection...")
-					self.socket.close()
-					return
+                                        response += data
 
-				self.socket.send((buffer + "\n").encode())
+                                print(response.replace(b"<END>", b"").decode(), end="")
 
-		except KeyboardInterrupt:
-			print("\nUser terminates.")
-			self.socket.close()
-			sys.exit()
+                                buffer = input("")
 
-	def listen(self):
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                                if buffer.lower() in ["exit", "quit", "q"]:
+                                        self.socket.send((buffer + "\n").encode())
+                                        print("Closing connection...")
+                                        self.socket.close()
+                                        return
 
-		self.socket.bind((self.args.target, self.args.port))
-		self.socket.listen(5)
+                                self.socket.send((buffer + "\n").encode())
 
-		print("[*] Server listening...")
+                except KeyboardInterrupt:
+                        print("\nUser terminates.")
+                        self.socket.close()
+                        sys.exit()
 
-		try:
-			while True:
-				client_socket, addr = self.socket.accept()
-				print(f"[*]\n[*] Connection from {addr[0]}:{addr[1]}")
+        def listen(self):
+                self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-				client_thread = threading.Thread(
-					target=self.handle,
-					args=(client_socket,),
-					daemon=True
-				)
-				client_thread.start()
+                self.socket.bind((self.args.target, self.args.port))
+                self.socket.listen(5)
 
-		except KeyboardInterrupt:
-			print("\n[!] Server shutting down...")
+                print("[*] Server listening...")
 
+                try:
 
-		finally:
-			self.socket.close()
-			print("[*] Socket closed cleanly.")
+                        while True:
+                                client_socket, addr = self.socket.accept()
+                                print(f"[*]\n[*] Connection from {addr[0]}:{addr[1]}")
 
-	def execute(self, cmd):
-		cmd = cmd.strip()
-		if not cmd:
-			return b""
+                                client_thread = threading.Thread(
+                                        target=self.handle,
+                                        args=(client_socket,),
+                                        daemon=True
+                                )
 
-		try:
-			output = subprocess.check_output(
-				cmd,
-				stderr=subprocess.STDOUT,
-				shell=True
-			)
-		except subprocess.CalledProcessError as e:
-			output = e.output or b"Command failed\n"
+                                client_thread.start()
 
-		return output
+                except KeyboardInterrupt:
+                        print("\n[!] Server shutting down...")
 
-	def handle(self, client_socket):
-		if self.args.execute:
-			output = self.execute(self.args.execute)
-			client_socket.send(output)
+                finally:
+                        self.socket.close()
+                        print("[*] Socket closed cleanly.")
 
-		elif self.args.upload:
-			file_buffer = b""
+        def execute(self, cmd):
+                cmd = cmd.strip()
 
-			while True:
-				data = client_socket.recv(4096)
+                if not cmd:
+                        return b""
 
-				if not data:
-					break
-				file_buffer += data
+                try:
+                        output = subprocess.check_output(
+                                cmd,
+                                stderr=subprocess.STDOUT,
+                                shell=True
+                        )
 
-			with open(self.args.upload, "wb") as f:
-				f.write(file_buffer)
+                except subprocess.CalledProcessError as e:
+                        output = e.output or b"\nCommand failed\n"
 
-			client_socket.send(b"File saved")
+                return output
 
-		else:
-			while True:
-				client_socket.send(b"<BHP: #> <END>")
-				cmd_buffer = b""
+        def handle(self, client_socket):
+                if self.args.execute:
+                        output = self.execute(self.args.execute)
 
-				while b"\n" not in cmd_buffer:
-					data = client_socket.recv(64)
+                        try:
+                                client_socket.send(output + b"<END>")
+                        except:
+                                pass
 
-					if not data:
-						return
+                        client_socket.close()
+                        return
 
-					cmd_buffer += data
+                elif self.args.upload:
+                        file_buffer = b""
 
-				cmd = cmd_buffer.decode().strip()
+                        while True:
+                                data = client_socket.recv(4096)
 
-				if cmd.lower() in ["exit", "quit", "q"]:
-					client_socket.send(b"Closing connection...\n")
-					client_socket.close()
-					return
+                                if data:
+                                        file_buffer += data
+                                else:
+                                        break
 
-				response = self.execute(cmd)
+                        with open(self.args.upload, "wb") as f:
+                                f.write(file_buffer)
 
-				if not response:
-					response = b""
+                        message = f'Saved file {self.args.upload}'
+                        client_socket.send(message.encode() + b"<END>")
 
-				client_socket.send(response + b"<END>")
+                else:
+                        while True:
+                                try:
+                                        client_socket.send(b"<BHP: #> <END>")
+                                except:
+                                        return
+
+                                cmd_buffer = b""
+
+                                while b"\n" not in cmd_buffer:
+                                        data = client_socket.recv(64)
+
+                                        if not data:
+                                                print("[*] Client disconnected")
+                                                return
+
+                                        cmd_buffer += data
+
+                                cmd = cmd_buffer.decode().strip()
+
+                                if cmd.lower() in ["exit", "quit", "q"]:
+                                        print("[*] Client requested disconnect")
+                                        try:
+                                                client_socket.send(b"\nClosing connection...\n")
+                                        except:
+                                                pass
+                                        client_socket.close()
+                                        return
+
+                                response = self.execute(cmd)
+
+                                if not response:
+                                        response = b""
+
+                                try:
+                                        client_socket.send(response + b"<END>")
+                                except:
+                                        return
+
 
 if __name__ == "__main__":
-	args = parse_args()
-	nc = Netcat(args)
+        args = parse_args()
+        nc = Netcat(args)
 
-	if args.listen:
-		nc.listen()
-	else:
-		nc.send()
+        if args.listen:
+                nc.listen()
+        else:
+                nc.send()
